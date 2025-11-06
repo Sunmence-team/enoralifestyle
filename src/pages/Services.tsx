a5e129b4806a50b509133eafe729ade3c81fa0e3
Abdulmuheez <abdulmuizayomide2@gmail.com>
2025-11-06 14:42:27 +0100

    omo

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -80,10 +80,8 @@ const Services = () => {
       />
 
       {/* SERVICES GRID */}
-      <div className="mt-10 lg:px-10 px-5">
-        <h1 className="md:text-[48px] text-[30px] text-center font-semibold mb-12">
-          <span className="text-[var(--primary-color)]">Our Services</span>
-        </h1>
+      <div className="mt-20 lg:px-10 px-5">
+
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {services.map((item, index) => (

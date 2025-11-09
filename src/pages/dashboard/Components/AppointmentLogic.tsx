29b51bad3f469818ee0d81ddcd7d5759a5a667c8
Salahudeen-femi1 <102353790+Salahudeen-femi1@users.noreply.github.com>
2025-11-09 19:35:00 +0100

    update on the dashboard

--- src/pages/dashboard/Components/AppointmentLogic.tsx
+++ src/pages/dashboard/Components/AppointmentLogic.tsx
@@ -9,31 +9,17 @@ export default function Appointments() {
   return (
     <div>
       <div className='flex justify-between items-center w-full mb-3'>
-        <h2 className='font-bold text-2xl'>Add Sercice</h2>
-
-        <div className='flex gap-3'>
-          <div className='flex items-center gap-2 text-sm rounded-full bg-white px-3 py-2 border border-[var(--primary-color)]'>
-            <input
-              type="search"
-              placeholder="Search"
-              className="w-full outline-none border-none bg-transparent placeholder:text-gray-400 text-gray-700"
-            />
-            <span>
-              <IoSearch size={20} className="text-gray-400" />
-            </span>
-          </div>
-          <div className=' flex gap-3 text-sm items-center rounded-full bg-white px- py-2 border border-(--primary-color) '>
-            <span><MdFilterList size={20} className='text-gray-400' /></span>
-            <input type="search" placeholder='Filter' className='' />
-          </div>
-
-
+        <div>
+          <h2 className='font-bold text-2xl'>Add Sercice</h2>
         </div>
 
-        <div className='flex gap-3'>
-          <div className='p-3 rounded-full bg-[var(--pink-color)] '><IoIosNotifications size={25} className='text-[var(--primary-color)]' /></div>
-          <div className='p-3 rounded-full bg-[var(--pink-color)]'><FaRegUserCircle size={25} className='text-[var(--primary-color)]' /></div>
+        <div>
+          
         </div>
+
+
+
+
       </div>
 
     </div>
